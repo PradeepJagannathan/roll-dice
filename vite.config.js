067fe8608af9dab19ca7dba905d401a11381e78e
Pradeep Jagannathan <n.janapradeep@gmail.com>
2025-06-12 20:15:10 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/roll-dice/"
+  base: "/roll-dice/",
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
 })
